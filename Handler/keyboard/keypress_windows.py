9f2b5f733ee66d6b9fa1062b3cf35b7c13ae4349
AdityaP1502 <i.m.aditya015@gmail.com>
2023-02-24 21:54:00 +0700

    thrown exception when ctrl + c is pressed

--- Handler/keyboard/keypress_windows.py
+++ Handler/keyboard/keypress_windows.py
@@ -7,6 +7,7 @@ class KeyPressHandlerWindows():
     end_key = keyboard.Key.esc
     
     def __init__(self, func) -> None:
+        self.exc = None
         self.is_paused=False  # pause keylog listener
         self.is_closed=False  # stop and close keylog
         self.l=None  # listener
@@ -15,22 +16,37 @@ class KeyPressHandlerWindows():
         self.focused_checker=Thread(target=self.check_focused)  # check if out of focused window in a thread
         self.focused_checker.start()
         
-        self.__func = func
+        self.__func = self.on_press_factory(func)
         self.start()
-
+      
     def join(self):
-        # stop and close keylog
         self.is_closed=True
         self.stop()
+        if self.exc != None:
+          raise self.exc
 
+    def on_press_factory(self, func):
+      def on_press(key):
+        try:
+          a = func(key)
+          if a == False:
+            return False
+        except Exception as e:
+          self.exc=e
+          self.is_closed=True
+          return False
+      
+      return on_press
+    
     def start(self):
         self.l=keyboard.Listener(on_press=self.__func)
         self.l.start()
         
     def stop(self):
-        # stop listener
-        self.l.stop()
-        self.l.join()
+        if self.l:
+          # stop listener
+          self.l.stop()
+          self.l.join()
         
     def check_focused(self):
         while not self.is_closed:
