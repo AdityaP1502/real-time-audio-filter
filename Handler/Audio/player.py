3c32071ba2812433f54739b29230f123a372d39a
AdityaP1502 <i.m.aditya015@gmail.com>
2023-03-01 19:10:18 +0700

    Fix overlap save implementation

--- Handler/Audio/player.py
+++ Handler/Audio/player.py
@@ -1,4 +1,6 @@
 from threading import Thread
+import traceback
+
 import pyaudio
 
 import Config.conf as conf
@@ -48,24 +50,28 @@ class AudioFilter():
         def increase_volume(self, data):
             return data * 10 ** (20 / conf.GAIN)
           
-        def filter_audio(self, raw_data : bytes):
-                data_arr = self.__convert_bytes_to_array(raw_data)
-                filtered_data = self.filter.filter(data_arr)
-                gain_data = map(lambda x:self.increase_volume(x), filtered_data)
+        def filter_audio(self, raw_datas : list[bytes]):
+            output_chunks = []
+            data0, data1 = map(lambda x: self.__convert_bytes_to_array(x), raw_datas)
+            filtered_data = self.filter.filter(data0, data1)
+            
+            for result in filtered_data:
+                gain_data = map(lambda x:self.increase_volume(x), result)
                 rounded_data = map(lambda x: self.__clamp_output(x), gain_data)
-                return self.__convert_arr_to_bytes(rounded_data)
-                # return raw_data
+                output_chunks.append(self.__convert_arr_to_bytes(rounded_data))
+                
+            return output_chunks
 
 class Player():         
     def __init__(self, buffer, audio_filter) -> None:
-        self.__thread = Thread(target = self.run)
+        self.__thread = Thread(target = self.run, daemon="True")
         self.__driver = pyaudio.PyAudio()
         self.stream = None
         self.buffer = buffer
         self.exc = None
         self.__thread.start()
         self.filter = audio_filter
-        # self.internal_buffer = []
+        self.internal_buffer = [] # to store two chunks of data
 
     def run(self):
         try:
@@ -83,15 +89,21 @@ class Player():
                 #     while len(self.internal_buffer):
                 #         data = self.internal_buffer.pop(0)
                 #         self.stream.write(data)
-                data = self.filter.filter_audio(data)
-                self.stream.write(data)
+                self.internal_buffer.append(data)
                 
+                if len(self.internal_buffer) == 2:
+                    filtered_data = self.filter.filter_audio(self.internal_buffer)
+                    for audio_data in filtered_data:
+                        self.stream.write(audio_data)
+                    
+                    self.internal_buffer = []
 
             self.stream.close()
             print("Player terminated!")
 
         except BaseException as e:
-            print(e)
+            print(traceback.format_exc())
+            print("Error in Player: {}".format(e))
             self.stream.close()
             self.exc = e
     
@@ -99,6 +111,9 @@ class Player():
         self.__thread.join()
         if self.exc != None:
             raise self.exc
+        
+    def is_active(self):
+        return self.__thread.is_alive()
     
     @classmethod 
     def init_player(cls, records, audio_filter):
--- Tools/Filter/DigitalFilter/FIR_Filter.py
+++ Tools/Filter/DigitalFilter/FIR_Filter.py
@@ -5,6 +5,7 @@ class FIR_Filter(Filter):
   def __init__(self, N, b0=None, zero=..., pole=..., hn=...) -> None:
     super().__init__(N, b0, zero, pole, hn)
     self.buffer = [0] * (N)
+    self.output_buffer = []
     self.__do_filter = None
     self.sample_length = None
 
@@ -14,19 +15,65 @@ class FIR_Filter(Filter):
     self.__do_filter = self.__filter_overlap_save if is_real_time else self.__filter_normal
     self.sample_length = sample_length
 
-  def __filter_overlap_save(self, xn):
-    xm = self.buffer + xn[0:self.sample_length - (self.N)]
-    assert len(xm) == self.sample_length, "the length of the block must eqaul to sample length {}. Received {}".format(self.sample_length, len(xm))
+  def __filter_overlap_save(self, xn_0, xn_1):
+    """ Filter using overlap save. Can create output up to 2 chunk of data. Need two input chunks 
+        to form the output chunks. 
+    Args:
+        xn_0 (list[float]): First chunk
+        xn_1 (list[float]): Second chunk
 
-    y = convfft_overlap_save(xm, self.hn)
-    self.buffer = xm[-(self.N):]
-    return y
+    Returns:
+        res : list[list[float]] : The output chunks
+    """
+    
+    assert type(xn_0).__name__ == "list", "input must have type list. Not {}".format(type(xn_0).__name__)
+    assert type(xn_1).__name__ == "list", "input must have type list. Not {}".format(type(xn_1).__name__)
+    
+    xm_0 = self.buffer + xn_0[0:self.sample_length - (self.N)]
+    xm_1 = xm_0[-(self.N):] + xn_1[0:self.sample_length - (self.N)]
+    
+    res = []
+    
+    assert len(xm_0) == self.sample_length, "the length of the block must eqaul to sample length {}. Received {}".format(self.sample_length, len(xm_0))
+    assert len(xm_1) == self.sample_length, "the length of the block must eqaul to sample length {}. Received {}".format(self.sample_length, len(xm_1))
+    
+    y0 = convfft_overlap_save(xm_0, self.hn)[self.N:] # discard M - 1 element
+    y1 = convfft_overlap_save(xm_1, self.hn)[self.N:] # discard M - 1 element
+    
+    # append data from the output buffer to y0
+    y = self.output_buffer + y0[0 : self.sample_length - (len(self.output_buffer))]
+    
+    # add data from the next output chunk
+    if len(y) < self.sample_length:
+      y = y + y1[0:self.sample_length - (len(y))]
+    
+    assert len(y) == self.sample_length, "Each output chunk must have {} length. Received {}".format(self.sample_length, len(y))  
+    res.append(y)
+    buffer_length = len(self.output_buffer) + len(y0) + len(y1) - self.sample_length
+    
+    # add lefover from the last and part of the next output to form another chunk
+    if buffer_length > self.sample_length:
+      y = y0[self.sample_length - (len(y)):]
+      y += y1[:self.sample_length - (len(y))]
+      buffer_length -= self.sample_length
+      assert len(y) == self.sample_length, "Each output chunk must have {} length. Received {}".format(self.sample_length, len(y))
+      res.append(y)
+    
+    # update buffer
+    self.output_buffer = y1[len(y1) - buffer_length: len(y1)]
+    
+    self.buffer = xm_1[-(self.N):]
+    
+    return res
   
   def __filter_normal(self, xn):
     y = convfft(xn, self.hn)
     return y
 
-  def filter(self, xn):
+  def filter(self, xn, xn_ = None):
+    if type(xn_).__name__ == "list":
+      return self.__do_filter(xn, xn_)
+    
     return self.__do_filter(xn)
 
     
