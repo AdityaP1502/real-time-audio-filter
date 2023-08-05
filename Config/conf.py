ae82bb03a0438c8177c5563b34b87725eae564ca
AdityaP1502 <i.m.aditya015@gmail.com>
2023-08-05 12:24:31 +0700

    Update config files

--- Config/conf.py
+++ Config/conf.py
@@ -4,18 +4,20 @@ import pyaudio
 # user system
 PLATFORM = platform.system()
 
+# FFT Engine
+FFT_ENGINE = "RADIX2"
+
 # Audio config files
-RATE = 44100 # in Hz
-CHUNK = 2048 # number of samples per block
-RECORD_SECONDS = 5 # only used when in blocking mode
-FORMAT = pyaudio.paInt16 # 16 bit per sample
+RATE = 44100  # in Hz
+CHUNK = 1024  # number of samples per block
+RECORD_SECONDS = 5  # only used when in blocking mode
+FORMAT = pyaudio.paInt16  # 16 bit per sample
 MAX_AUDIO_VALUES = 32767
 MIN_AUDIO_VALUES = -32768
-CHANNELS = 1 # number of audio channels
-GAIN = 100 # in db
+CHANNELS = 1  # number of audio channels
+GAIN = 100  # in db
 
 # keyboard event
 START_RECORDING = 'v'
-END_PROGRAM = chr(27) # esc
-
-
+END_RECORDING = 'c'
+END_PROGRAM = chr(27)  # esc
